392edee1df3576f14ff12cc795d33c71809eef01
alysoncp <alyson.c.pickett@gmail.com>
2020-12-16 12:36:08 -0800

    Wednesday's work

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -16,9 +16,9 @@ export default function useApplicationData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     })
