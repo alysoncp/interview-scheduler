fccb357df2acd4a536e0ff72d2c972935561acf6
alysoncp <alyson.c.pickett@gmail.com>
2020-12-16 19:57:57 -0800

    caniuse bs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "index.scss";
+import Application from "components/Application";
 
 import axios from "axios";
 
@@ -10,6 +11,5 @@ if (process.env.REACT_APP_API_BASE_URL) {
 }
 
 
-import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
